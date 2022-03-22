130ec877f252408396ccd5ba3379224d0c1bdb1b
Daniel Xu <dxu@dxuuu.xyz>
2022-03-21 17:48:48 -0700

    Export ReviewComment fields

--- src/parser.rs
+++ src/parser.rs
@@ -6,18 +6,18 @@ pub struct ReviewComment {
     /// File the comment is in
     ///
     /// Note that this is the new filename if the file was also moved
-    file: String,
+    pub file: String,
     /// The "line" a comment applies to. To quote github API:
     ///
     /// The position value equals the number of lines down from the first "@@" hunk header in the
     /// file you want to add a comment. The line just below the "@@" line is position 1, the next
     /// line is position 2, and so on. The position in the diff continues to increase through lines
     /// of whitespace and additional hunks until the beginning of a new file.
-    position: u64,
+    pub position: u64,
     /// For a spanned comment, the first line of the span. See `position` for docs on semantics
-    start_position: Option<u64>,
+    pub start_position: Option<u64>,
     /// The user-supplied review comment
-    comment: String,
+    pub comment: String,
 }
 
 struct FilePreambleState {
